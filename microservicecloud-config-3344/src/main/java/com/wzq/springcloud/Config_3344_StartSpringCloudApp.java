5167e83e7f02afa253e155f0e07dd7fb9f52aea3
wangzq95928 <898603565@qq.com>
2019-10-15 14:52:28 +0800

    修改描述

---
+++ microservicecloud-config-3344/src/main/java/com/wzq/springcloud/Config_3344_StartSpringCloudApp.java
@@ -0,0 +1,19 @@
+package com.wzq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: ect
+ * @DATE 2019-10-15
+ * @descripttion:
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class Config_3344_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Config_3344_StartSpringCloudApp.class, args);
+    }
+}
